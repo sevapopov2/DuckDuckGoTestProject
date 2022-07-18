46f37a5731b148bf24de5801cd7c89abedb689b0
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-18 18:08:01 +0300

    Add search and result page locators

--- pages/search.py
+++ pages/search.py
@@ -1,11 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
 class DuckDuckGoSearchPage:
+    # URL
+    URL = 'https://duckduckgo.com'
+    # Locators
+    SEARCH_INPUT = (By.XPATH, "//input[@id='search_form_input_homepage']")
+
     def __init__(self, browser):
         self.browser = browser
 
     def load(self):
-        #TODO
-        pass
+        self.browser.get(self.URL)
 
     def search(self, phrase):
-        #TODO
-        pass
+        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input.send_keys(phrase + Keys.RETURN)
