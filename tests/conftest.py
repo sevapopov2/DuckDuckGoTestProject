9649efd0a0ed7181234bda8b121f43edbd1a7a93
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-22 11:45:07 +0300

    Add json config file to remove hardcodings in code

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,31 @@
+import json
 import pytest
 import selenium.webdriver
 
+
 @pytest.fixture
-def browser():
-    web_browser = selenium.webdriver.Chrome()
-    web_browser.implicitly_wait(10)
+def config(scope='session'):
+    # Read json file with parameters
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    assert config['browser'] in ['Firefox', 'Chrome', 'Headless Chrome']
+    assert isinstance(config['implicit_wait'], int)
+    assert config['implicit_wait'] > 0
+    return config
+
+
+@pytest.fixture
+def browser(config):
+    if config['browser'] == 'Firefox':
+        web_browser = selenium.webdriver.Firefox()
+    elif config['browser'] == 'Chrome':
+        web_browser = selenium.webdriver.Chrome()
+    elif config['browser'] == 'Headless Chrome':
+        opts = selenium.webdriver.ChromeOptions()
+        opts.add_argument('headless')
+        web_browser = selenium.webdriver.Chrome(options=opts)
+    else:
+        raise Exception(f'Browser "{config["browser"]}" is not supported')
+    web_browser.implicitly_wait(config['implicit_wait'])
     yield web_browser
     web_browser.quit()
