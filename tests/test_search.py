a15c264a1b27f07f5874ae1830e79dbb0739b402
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-22 13:05:59 +0300

    Add parametrized test to search for several phrases

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,17 +1,18 @@
 from pages.search import DuckDuckGoSearchPage
 from pages.result import DuckDuckGoResultPage
+import pytest
 
 
-def test_basic_duckduckgo_search(browser):
+@pytest.mark.parametrize('phrase', ['panda', 'python', 'cat'])
+def test_basic_duckduckgo_search(browser, phrase):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
-    PHRASE = "panda"
     # Load the page
     search_page.load()
     # Search for panda
-    search_page.search(PHRASE)
-    assert PHRASE == result_page.search_input_value()
+    search_page.search(phrase)
+    assert phrase == result_page.search_input_value()
     titles = result_page.result_link_titles()
-    matches = [t for t in titles if PHRASE.lower() in t.lower()]
+    matches = [t for t in titles if phrase.lower() in t.lower()]
     assert len(matches) > 0
-    assert PHRASE in result_page.title()
+    assert phrase in result_page.title()
