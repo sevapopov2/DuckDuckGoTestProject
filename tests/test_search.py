a04811138d5f0eb360415b585890dc0edc302a22
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-22 12:06:22 +0300

    Remove incomplete exception in search test

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,14 +6,12 @@ def test_basic_duckduckgo_search(browser):
     search_page = DuckDuckGoSearchPage(browser)
     result_page = DuckDuckGoResultPage(browser)
     PHRASE = "panda"
-    #Load the page
+    # Load the page
     search_page.load()
-    #Search for panda
+    # Search for panda
     search_page.search(PHRASE)
     assert PHRASE in result_page.title()
     assert PHRASE == result_page.search_input_value()
     titles = result_page.result_link_titles()
     matches = [t for t in titles if PHRASE.lower() in t.lower()]
     assert len(matches) > 0
-
-    raise Exception("Incomplete test")
