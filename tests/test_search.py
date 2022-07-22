22bb7aa50a4521a370ffebf956851fad6aa4b99e
Vsevolod Popov <sevapopov13@gmail.com>
2022-07-22 12:53:44 +0300

    Change the title check assertion for test to work in Firefox

--- tests/test_search.py
+++ tests/test_search.py
@@ -10,8 +10,8 @@ def test_basic_duckduckgo_search(browser):
     search_page.load()
     # Search for panda
     search_page.search(PHRASE)
-    assert PHRASE in result_page.title()
     assert PHRASE == result_page.search_input_value()
     titles = result_page.result_link_titles()
     matches = [t for t in titles if PHRASE.lower() in t.lower()]
     assert len(matches) > 0
+    assert PHRASE in result_page.title()
